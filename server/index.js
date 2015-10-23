3b62298b1d8e4d4d241af520954e43e01f731ac9
Ovidio R. Calvet <ovidio@calvetsolutions.com>
2015-10-22 22:56:54 -0400

    Added a local database to be able to persist users

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var engine = require('ejs-mate');
+var bodyParser = require('body-parser');
+var Datastore = require('nedb');
+var users = new Datastore({ filename: 'db/users.db', autoload: true });
+
+// Set body parser
+app.use(bodyParser.json());
 
 // Set ejs as the view engine
 app.engine('ejs', engine);
@@ -17,12 +23,43 @@ app.get('/todo', function(req, res) {
 	res.render('index');
 });
 
+app.get('/user', function(req, res) {
+	users.find({}, function(err, users) {
+		if (err) {
+			res.status(404).json({ message: 'There was a problem loading users'});
+		} else {
+			res.json(users);
+		}
+	});
+});
+
 app.post('/login', function(req, res) {
 	res.json({
-		user: { name: 'Ovidio R. Calvet' },
+		user: req.body,
 		access_token: 'adskasdjalksdme.mdemelekmekljfoaiejpdk[aepedlapedealp'
 	})
 });
+
+app.post('/register', function(req, res) {
+	var user = req.body;
+	
+	var newUser = {
+		name: user.name,
+		email: user.email,
+		password: user.password
+	};
+	
+	users.insert(newUser, function(err, user) {
+		if (err) {
+			res.status(404).json({ message: 'There was a problem inserting user'});
+		} else {
+			res.json({
+				user: user,
+				access_token: "sdadjkasjdksadsa.djasldkladj;alsfkal;dklsdafklasfk"
+			});
+		}
+	});
+})
 
 app.use(express.static('public'));
 
